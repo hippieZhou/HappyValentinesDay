0f01bd73418a0227ac8d62b4915a72541972ab00
hippieZhou <hippiezhou@outlook.com>
2020-08-28 23:36:26 +0800

    update

--- src/HappyValentinesDay/MainView.xaml.cs
+++ src/HappyValentinesDay/MainView.xaml.cs
@@ -2,10 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
@@ -15,9 +17,11 @@ namespace HappyValentinesDay
 {
     public partial class MainView : Window
     {
+        private readonly MediaPlayer _mp;
         public MainView()
         {
             InitializeComponent();
+            _mp = new MediaPlayer();
         }
 
         protected override void OnClosed(EventArgs e)
@@ -29,6 +33,8 @@ namespace HappyValentinesDay
 
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            PlayRandomMusic(0);
+
             _tokenSource = new CancellationTokenSource();
 
             await StartHeartAsync(PetalBackground);
@@ -37,18 +43,18 @@ namespace HappyValentinesDay
             var span = DateTime.Now - startTime;
             var data = new string[]
             {
+                "亲爱的老婆大人，❤",
+                "七夕快乐哟哟哟 ~ ~",
                 $"经历了 {(int)span.TotalDays} 天",
                 $"熬过了 {(int) span.TotalHours} 个小时",
-                "宝贝，❤",
                 "和你在一起的每时每刻我都倍感珍惜",
                 "我想你，绝不仅仅是想你",
                 "而是想这辈子都和你在一起",
-                "不曾对爱情有过过多的奢求",
+                "不曾对爱情有过太多的奢求",
                 "这辈子有你足矣",
                 "一想到能和你共度余生",
                 "我就对余生充满了期待",
-                "我爱你，直至",
-                "天荒地老……"
+                "Start with you, no end……",
             };
             await StartTextAsync(data, Tb);
         }
@@ -94,12 +100,18 @@ namespace HappyValentinesDay
                                  return;
 
                              #region 创建随机爱心
-                             int width = random.Next(10, 40);
+                             int width = random.Next(10, 100);
                              PetalControl heart = new PetalControl
                              {
+                                 Cursor = Cursors.Hand,
                                  Width = width,
                                  Height = width,
                                  RenderTransform = new RotateTransform()
+                             };
+                             heart.MouseDoubleClick += (sender, e) => 
+                             {
+                                 var index = random.Next(0, MusicList.Count() - 1);
+                                 PlayRandomMusic(index);
                              };
 
                              int left = random.Next(0, (int)panel.ActualWidth);
@@ -108,7 +120,7 @@ namespace HappyValentinesDay
                              #endregion
 
                              #region 创建渐变动画
-                             int seconds = random.Next(20, 30);
+                             int seconds = random.Next(30, 60);
                              heart.BeginAnimation(OpacityProperty, new DoubleAnimation(0, 1, TimeSpan.FromSeconds(1)));
                              #endregion
 
@@ -135,6 +147,35 @@ namespace HappyValentinesDay
                      }, DispatcherPriority.Normal, _tokenSource.Token);
                  }
              }, _tokenSource.Token);
+        }
+
+        private static IEnumerable<string> MusicList = new List<string>
+        {
+            "http://218.205.239.34/MIGUM2.0/v1.0/content/sub/listenSong.do?toneFlag=LQ&netType=00&copyrightId=0&contentId=600910000009613592&resourceType=2&channel=0",
+            "http://218.205.239.34/MIGUM2.0/v1.0/content/sub/listenSong.do?toneFlag=LQ&netType=00&copyrightId=0&contentId=600908000000398656&resourceType=2&channel=0",
+            "http://218.205.239.34/MIGUM2.0/v1.0/content/sub/listenSong.do?toneFlag=LQ&netType=00&copyrightId=0&contentId=600902000002560500&resourceType=2&channel=0",
+            "http://218.205.239.34/MIGUM2.0/v1.0/content/sub/listenSong.do?toneFlag=LQ&netType=00&copyrightId=0&contentId=600914000002097907&resourceType=2&channel=0",
+            "http://218.205.239.34/MIGUM2.0/v1.0/content/sub/listenSong.do?toneFlag=LQ&netType=00&copyrightId=0&contentId=600902000006889066&resourceType=2&channel=0",
+            "http://218.205.239.34/MIGUM2.0/v1.0/content/sub/listenSong.do?toneFlag=LQ&netType=00&copyrightId=0&contentId=600902000009218893&resourceType=2&channel=0",
+            "http://218.205.239.34/MIGUM2.0/v1.0/content/sub/listenSong.do?toneFlag=HQ&netType=00&copyrightId=0&contentId=600913000009207857&resourceType=2&channel=0",
+            "http://218.205.239.34/MIGUM2.0/v1.0/content/sub/listenSong.do?toneFlag=LQ&netType=00&copyrightId=0&contentId=600910000005536461&resourceType=2&channel=0",
+            "http://218.205.239.34/MIGUM2.0/v1.0/content/sub/listenSong.do?toneFlag=LQ&netType=00&copyrightId=0&contentId=600908000008814792&resourceType=2&channel=0",
+            "http://218.205.239.34/MIGUM2.0/v1.0/content/sub/listenSong.do?toneFlag=LQ&netType=00&copyrightId=0&contentId=600913000006669361&resourceType=2&channel=0",
+            "http://218.205.239.34/MIGUM2.0/v1.0/content/sub/listenSong.do?toneFlag=LQ&netType=00&copyrightId=0&contentId=600908000008738879&resourceType=2&channel=0",
+            "http://218.205.239.34/MIGUM2.0/v1.0/content/sub/listenSong.do?toneFlag=LQ&netType=00&copyrightId=0&contentId=600913000004733076&resourceType=2&channel=0",
+            "http://218.205.239.34/MIGUM2.0/v1.0/content/sub/listenSong.do?toneFlag=LQ&netType=00&copyrightId=0&contentId=600907000004656284&resourceType=2&channel=0",
+            "http://218.205.239.34/MIGUM2.0/v1.0/content/sub/listenSong.do?toneFlag=SQ&netType=00&copyrightId=0&contentId=600916000005730738&resourceType=E&channel=0",
+            "http://freetyst.nf.migu.cn/public/product7th/productB16/2020/07/1418/2019%E5%B9%B412%E6%9C%8818%E6%97%A508%E7%82%B910%E5%88%86%E6%89%B9%E9%87%8F%E9%A1%B9%E7%9B%AE%E5%92%AA%E5%92%95Believe179%E9%A6%96-6/%E5%85%A8%E6%9B%B2%E8%AF%95%E5%90%AC/Mp3_64_22_16/6993160VZDN184529.mp3?channelid=03&k=d320f7c6b58589ff&t=1598364024&msisdn=0fbc4fe7-89a5-4a19-920d-35ba05251a87",
+        };
+        private void PlayRandomMusic(int index)
+        {
+            _mp.Close();
+
+            var music = MusicList.ElementAt(index);
+            var source = new Uri(music);
+            _mp.Open(source);
+            _mp.MediaEnded += (_s, _e) => { _mp.Position = TimeSpan.FromSeconds(0); _mp.Play(); };
+            _mp.Play();
         }
     }
 }
